ab8b7f09010efd234ae0d4a101ce00c5c7e6c510
Nikita <nikit.zakharov@gmail.com>
2022-12-08 14:56:32 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+using SFModule8;
+using System;
+using System.Globalization;
+using System.Net;
+using System.Reflection.PortableExecutable;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
+
+namespace SFModule8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
